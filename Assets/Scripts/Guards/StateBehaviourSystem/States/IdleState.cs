313b51015b65d3e733af26fbe941807b92b32f6b
Jasper in 't Veld <jasperintveld@hotmail.nl>
2023-12-18 12:54:46 +0000

    temp commit

--- Assets/Scripts/Guards/StateBehaviourSystem/States/IdleState.cs
+++ Assets/Scripts/Guards/StateBehaviourSystem/States/IdleState.cs
@@ -4,27 +4,49 @@ using Random = UnityEngine.Random;
 
 public class IdleState : AStates
 {
-    //The chance of changing state where 1 is 1% and 100 is 100%
+    //The chance of changing state where 0 is 0% and 100 is 100%
     [SerializeField]
-    [Range(1, 100)]
+    [Range(0, 100)]
     private float _changeStateChance = 5;
+
+    //animation values
+    private float _initialSpeed;
+    private float _targetSpeed;
+
+    [SerializeField]
+    private float _timeForTransition = 1.0f;
+    private float _timer;
 
     public override bool InitializeState()
     {
+        _targetSpeed = 0.0f;
+
         return true;
     }
 
     public override void OnStateStart()
     {
         Debug.Log("<color=cyan>Entering Idle State</color>");
+
+        AssociatedStateMachine.Agent.speed = 0;
+
+        _timer = 0.0f;
+
+        _initialSpeed = AssociatedStateMachine.GuardAnimator.GetFloat("MovementSpeed");
+
+        AssociatedStateMachine.GuardAnimator.SetFloat("RotationSpeed", 0.5f);
     }
 
     public override void OnStateUpdate()
     {
+        _timer = Mathf.Clamp01(_timer + Time.deltaTime / _timeForTransition);
+        AssociatedStateMachine.GuardAnimator.SetFloat("MovementSpeed", Mathf.Lerp(_initialSpeed, _targetSpeed, _timer));
     }
 
     public override void OnStateEnd()
     {
+        AssociatedStateMachine.GuardAnimator.SetFloat("MovementSpeed", 0.0f);
+        AssociatedStateMachine.GuardAnimator.SetFloat("RotationSpeed", 0.5f);
         Debug.Log("<color=cyan>Exiting Idle State</color>");
     }
 
@@ -33,8 +55,11 @@ public class IdleState : AStates
         var number = Random.Range(1, 100);
 
         bool seenPlayer = AssociatedStateMachine.LineOfSight.SeenObject();
-
-        if (seenPlayer) return (int) Config.States.Chase;
+        if (seenPlayer)
+        {
+            AssociatedStateMachine.GuardAnimator.Play("Pointing");
+            return (int) Config.States.Chase;
+        }
         if (number <= _changeStateChance) return (int) Config.States.Patrol;
         return (int) Config.States.Invalid;
     }
--- Assets/Scripts/Guards/StateBehaviourSystem/States/PatrolState.cs
+++ Assets/Scripts/Guards/StateBehaviourSystem/States/PatrolState.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 [RequireComponent(typeof(Transform))]
 public class PatrolState : AStates
@@ -8,11 +10,23 @@ public class PatrolState : AStates
 
     private int _target;
 
-    private Transform _transform;
+    [SerializeField] 
+    private float _walkingSpeed = 1.0f;
+
+    //animation values
+    private float _initialSpeed;
+    private float _targetSpeed;
+    
+    [SerializeField]
+    private float _timeForTransition = 1.0f;
+    private float _timer;
+
+    private bool _hasTurned;
+
 
     public override bool InitializeState()
     {
-        _transform = GetComponent<Transform>();
+        _targetSpeed = 0.5f;
 
         return true;;
     }
@@ -21,23 +35,38 @@ public class PatrolState : AStates
     {
         Debug.Log("<color=green>Entering Patrol State</color>");
         SetTarget();
+
+        AssociatedStateMachine.Agent.speed = 0;
+
+        _initialSpeed = AssociatedStateMachine.GuardAnimator.GetFloat("MovementSpeed");
+        _timer = 0.0f;
+
+        _hasTurned = false;
+
+        MoveToTarget();
     }
 
     public override void OnStateUpdate()
     {
-        MoveToTarget();
+        Animate();
     }
 
     public override void OnStateEnd()
     {
+        AssociatedStateMachine.GuardAnimator.SetFloat("MovementSpeed", 0.5f);
         Debug.Log("<color=green>Exiting Patrol State</color>");
     }
 
     public override int StateTransitionCondition()
     {
         bool seenPlayer = AssociatedStateMachine.LineOfSight.SeenObject();
-        
-        if (seenPlayer) return (int) Config.States.Chase;
+
+        if (seenPlayer)
+        {
+            AssociatedStateMachine.GuardAnimator.Play("Pointing");
+            AssociatedStateMachine.GuardAnimator.SetFloat("MovementSpeed", 1);
+            return (int) Config.States.Chase;
+        }
         if (IsAtLocation()) return (int) Config.States.Idle;
         return (int) Config.States.Invalid;
     }
@@ -64,5 +93,54 @@ public class PatrolState : AStates
         var currentLocation = transform.position;
 
         return Vector3.Distance(targetLocation, currentLocation) < 0.1f;
+    }
+
+    private void Animate()
+    {
+        if (_hasTurned)
+        {
+            _timer = Mathf.Clamp01(_timer + Time.deltaTime / _timeForTransition);
+            AssociatedStateMachine.GuardAnimator.SetFloat("MovementSpeed", Mathf.Lerp(_initialSpeed, _targetSpeed, _timer));
+            AssociatedStateMachine.Agent.speed = _walkingSpeed;
+        }
+        else
+        {
+            //Calculate the angle towards the target
+            //If the target is behind the guard, change the RotationSpeed of the animator so that the guard turns towards the target (0.0f is left, 1.0f is right turn angle)
+            //If the target is somewhere in front of the guard, he doesnt have to turn (0.5f)
+
+            var targetLocation = PointOfInterests[_target].transform.position;
+            var currentLocation = transform.position;
+
+            var direction = targetLocation - currentLocation;
+
+            var angle = Vector3.Angle(transform.forward, direction);
+
+            //If angle is greater than 90 degrees, the target is behind the guard
+            //And if is less than 270 degrees, the target is behind the guard
+
+            if (angle < 90 && angle > 270)
+            {
+                _hasTurned = true;
+            }
+            else
+            {
+                float targetDirection;
+
+                if (angle < 180) targetDirection = 1.0f; //turn right
+                else targetDirection = 0.0f; //turn left
+
+                _timer = Mathf.Clamp01(_timer + Time.deltaTime / (_timeForTransition * 2));
+                AssociatedStateMachine.GuardAnimator.SetFloat("RotationSpeed", Mathf.Lerp(0.5f, targetDirection, _timer));
+                Debug.Log(AssociatedStateMachine.GuardAnimator.deltaRotation);
+                //transform.rotation = AssociatedStateMachine.GuardAnimator.deltaRotation;
+
+                if (_timer >= 1.0f)
+                {
+                    _timer = 0.0f;
+                    _hasTurned = true;
+                }
+            }
+        }
     }
 }
